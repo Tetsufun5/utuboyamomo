7e9e97cfb8622f489da7b1a6b95b03e40c8223a8
Tetsufun5 <v1c.firth1235@gmail.com>
2025-04-24 14:07:39 +0900

    Update script

--- js/script.js
+++ js/script.js
@@ -25,31 +25,12 @@ $(window).on('load resize', function() {
     } 
     });
 
-//スクロールをするとハンバーガーメニューに変化するための設定を関数でまとめる
-function FixedAnime() {
-	//ヘッダーの高さを取得
-	var headerH = $('#header').outerHeight(true);
-	var scroll = $(window).scrollTop();
-	if (scroll >= headerH){//ヘッダーの高さ以上までスクロールしたら
-			$('.openbtn').addClass('fadeDown');//.openbtnにfadeDownというクラス名を付与して
-			$('.areaCmnheader').addClass('dnone');//#headerにdnoneというクラス名を付与
-		}else{//それ以外は
-			$('.openbtn').removeClass('fadeDown');//fadeDownというクラス名を除き
-			$('.areaCmnheader').removeClass('dnone');//dnoneというクラス名を除く
-		}
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-	FixedAnime();//スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
-});
-
 //ボタンをクリックした際のアニメーションの設定
-$(".openbtn").click(function () {//ボタンがクリックされたら
+$(".btn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $(".areaCmnheader").toggleClass('panelactive');//ヘッダーにpanelactiveクラスを付与
+    $(".header").toggleClass('panelactive');//ヘッダーにpanelactiveクラスを付与
 });
 $("#g-navi li a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $(".areaCmnheader").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
+    $(".header").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
+    $(".btn").removeClass('active');//ボタンの activeクラスを除去し
 });
